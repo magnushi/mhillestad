4c08293b33ddc0e32b6801d2edc26334f6aaebf9
Magnus Hillestad <magnus.hillestad@gmail.com>
2020-08-15 11:18:14 -0700

    updated bio

--- mhillestad/src/App.js
+++ mhillestad/src/App.js
@@ -2,12 +2,12 @@ import React from 'react';
 import './App.css';
 
 function App (){
-  const me = 'Magnus'
+  const me = 'Magnus Hillestad'
   
   return (
     <div className = 'main'>
-    <h1> Magnus Hillestads Homepage </h1>
-    <h1> I'm {me}. I'm CEO and Co-founder of Sanity. </h1>
+    <h1> {me} Homepage </h1>
+    <h1> I'm CEO and Co-founder of Sanity. Living in East Bay of San Francisco.</h1>
     </div>
   )
 };
