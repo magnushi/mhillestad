dc640ac18bf48b517c3329abaf32c73ef09ec41c
Magnus Hillestad <magnus.hillestad@gmail.com>
2020-08-15 11:10:36 -0700

    first commit

---
+++ mhillestad/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+function App (){
+  const me = 'Magnus'
+  
+  return (
+    <div className = 'main'>
+    <h1> Magnus Hillestads Homepage </h1>
+    <h1> I'm {me}. I'm CEO and Co-founder of Sanity. </h1>
+    </div>
+  )
+};
+
+
+export default App;
